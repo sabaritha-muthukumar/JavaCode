337bf63b19ef14501b61934150e3b35f4cf57101
SabarithaMuthukumar <50823949+SabarithaMuthukumar@users.noreply.github.com>
2019-07-06 21:41:40 +0530

    Add files via upload

---
+++ oneOnePattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class oneOnePattern
+{
+public static void main(String args[])
+{
+
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=0;
+for(int i=1;i<=a;i++)
+{
+System.out.println((int)Math.pow(11,i));
+}
+}
+}
